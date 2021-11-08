96c899cd3ac10ca9074a723de79a22fca8f25019
hankei6km <gh-hankei6km@outlook.jp>
2021-11-08 12:46:06 +0900

    Setup

---
+++ README_ja.md
@@ -0,0 +1,15 @@
+# rehype-image-salt-doc
+
+[rehype-image-salt](https://github.com/hankei6km/rehype-image-salt) のドキュメントをデプロイするリポジトリ。
+
+## 概要
+
+- [nuxt-content Docs Theme](https://content.nuxtjs.org/themes/docs) で作成
+- 画像表示には [nuxt-image](https://image.nuxtjs.org/) と [imgix](https://docs.imgix.com/) を利用
+- コンテンツは [remote-cms-contents](https://github.com/hankei6km/remote-cms-contents) でリモート CMS から取得
+
+## ライセンス
+
+MIT License
+
+Copyright (c) 2021 hankei6km
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,65 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
   docs: {
-    primaryColor: '#E24F55'
-  }
-})
+    primaryColor: "#3B82F6",
+  },
+  i18n: {
+    locales: () => [
+      {
+        code: "ja",
+        iso: "ja-JP",
+        file: "ja-JP.js",
+        name: "日本語",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en-US.js",
+        name: "English",
+      },
+    ],
+    defaultLocale: "ja",
+  },
+  buildModules: ["@nuxt/image"],
+  modules: ["@nuxt/image"],
+  image: {
+    // domains: [ ],
+    imgix: {
+      baseURL: "https://images.microcms-assets.io/",
+    },
+  },
+  pwa: {
+    // これを設定しておかないと "static/icon.png" を置き換えても
+    // デフォルトのアイコン画像が使われる
+    icon: {
+      source: "static/icon.png",
+    },
+  },
+  content: {
+    markdown: {
+      rehypePlugins: [
+        [
+          "@hankei6km/rehype-image-salt",
+          {
+            baseURL: "https://images.microcms-assets.io/",
+            rebuild: {
+              tagName: "nuxt-img",
+              baseAttrs: 'provider="imgix" data-salt-q=""',
+            },
+          },
+        ],
+      ],
+    },
+  },
+  server: {
+    // host: 0,
+    host: process.env.NODE_ENV !== "production" ? "0" : "localhost", // デフォルト: localhost
+  },
+  router: {
+    base: process.env.BASE_PATH || "",
+  },
+  publicRuntimeConfig: {
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
+  },
+});
